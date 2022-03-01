c4d837adb581e6246342d0dfed6e5bdc50e3aec4
hashibadaiki <higemeganeking@gmail.com>
2022-03-01 14:02:38 +0900

    atom を元に戻した

--- src/components/atoms/Atom.tsx
+++ src/components/atoms/Atom.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { useTimeout } from '../../hooks/Timeout';
 
 const test = 'test';
 
 export const Atom = () => {
-  const [seconds, setSeconds] = React.useState(0);
-  useTimeout(() => {
-    setSeconds(seconds + 2);
-  }, 5000);
-
   return (
     <div>
       <div>Atom</div>
       <div>{test}</div>
-      <div>{seconds}</div>
     </div>
   );
 };
